76f0a4e3c78c4792dd8a1db35832f5b0f8180d43
Fredirak <65224702+Fredirak@users.noreply.github.com>
2025-05-20 02:11:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.static('public'));
