0c6e4f03011f5e566eb7b95d7fdbae62c7daca75
Fredirak <65224702+Fredirak@users.noreply.github.com>
2025-05-20 02:59:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,8 @@ app.post('/reset', (req, res) => {
   const data = {
     Frieder: { dailyCount: 0, totalCount: 0 },
     Gesine: { dailyCount: 0, totalCount: 0 },
-    Fredi: { dailyCount: 0, totalCount: 0 }
+    Fredi: { dailyCount: 0, totalCount: 0 },
+    Lea: { dailyCount: 0, totalCount: 0 }
   };
   fs.writeFileSync('./data.json', JSON.stringify(data, null, 2));
   res.send('Alle Counter wurden zurückgesetzt!');
